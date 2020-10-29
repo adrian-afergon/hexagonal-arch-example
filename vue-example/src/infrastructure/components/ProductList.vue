66770f6903b389f8e376d6ece61939c754d29fd1
Adrian Ferrera <adrian.afergon@gmail.com>
2020-10-29 13:11:42 +0000

    feat: display product list on both applications

---
+++ vue-example/src/infrastructure/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="product in products " :key="product.id"> {{ product.title }}</li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { getProducts } from '@/domain/repositories/product.repository'
+import { Product } from '@/domain/models/Product'
+
+export default defineComponent({
+  name: 'ProductList',
+  data () {
+    return {
+      products: [] as Product[]
+    }
+  },
+  mounted () {
+    getProducts().then(response => (this.products = response))
+  }
+})
+</script>
