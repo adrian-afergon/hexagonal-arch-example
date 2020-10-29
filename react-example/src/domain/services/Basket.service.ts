617d475e3a6843656ccaea42b1dc012a24e28443
Adrian Ferrera <adrian.afergon@gmail.com>
2020-10-29 11:13:45 +0000

    chore: create services

---
+++ react-example/src/domain/services/Basket.service.ts
@@ -0,0 +1,22 @@
+import * as uuid from 'uuid'
+import { Product } from '../models/Product'
+import { Basket } from '../models/Basket'
+
+const hasProduct = (basket: Basket, product: Product) => basket.items.find((item) => item.id === product.id)
+
+const createBasket = (product: Product) => ({
+  id: uuid.v4(),
+  items: [product]
+})
+
+const increaseBasket = (basket: Basket, product: Product) => ({
+  ...basket,
+  items: [...basket.items, product]
+})
+
+export const addProductToBasket = (product: Product, basket?: Basket): Basket =>
+  basket
+    ? hasProduct(basket, product)
+        ? basket
+        : increaseBasket(basket, product)
+    : createBasket(product)
